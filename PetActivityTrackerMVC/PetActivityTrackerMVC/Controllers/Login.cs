7851571a6e7b46faa32ccf82056b7a908ae18361
wtotokshau1 <54911613+wtotokshau1@users.noreply.github.com>
2022-03-16 09:11:21 -0600

    V0.0
    Project created and added to source control

---
+++ PetActivityTrackerMVC/PetActivityTrackerMVC/Controllers/Login.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetActivityTrackerMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetActivityTrackerMVC.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Users user)
+        {
+            if (user.UserName == "Warren" && user.Password == "Password")
+                return View("PetDetails");
+            else
+                return View();
+        }
+    }
+}
---
+++ PetActivityTrackerMVC/PetActivityTrackerMVC/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetActivityTrackerMVC.Models
+{
+    public class Users
+    {
+        [Required(ErrorMessage ="Username is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
