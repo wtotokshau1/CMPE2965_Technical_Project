e4f10c7ad52b4362360203830a4bef191e3c42c1
wtotokshau1 <54911613+wtotokshau1@users.noreply.github.com>
2022-03-17 12:41:12 -0600

    Updated Database Creation script
    -Fixed Syntax errors
    -Added more columns to tables

--- PetActivityTestDB.sql
+++ PetActivityTestDB.sql
@@ -1,55 +1,60 @@
---Create User table
+--Drop Tables
+DROP TABLE IF EXISTS dbo.PetActivity
+DROP TABLE IF EXISTS dbo.Pet
 DROP TABLE IF EXISTS dbo.Users
+
+
+--Create User table
 CREATE TABLE Users(
 	UserID int NOT NULL IDENTITY,
 	UserName varchar(50) NOT NULL,
 	Password varchar(50) NOT NULL,
 	CONSTRAINT PK_Users PRIMARY KEY (UserID)
-);
+)
 GO
 
 --Create Pet Table
-DROP TABLE IF EXISTS dbo.Pet
 CREATE TABLE Pet(
 	PetID int NOT NULL,
 	UserID int NOT NULL,
 	PetName varchar(50) NOT NULL,
+	FoodConsumption int,
+	WaterConsumption int,
 	CONSTRAINT PK_Pet PRIMARY KEY (PetID, UserID),
 	CONSTRAINT FK_Pet_Users FOREIGN KEY (UserID) REFERENCES Users(UserID)
-);
+)
 GO
 
 --Create Pet Activity Table
-DROP TABLE IF EXISTS dbo.PetActivity
 CREATE TABLE PetActivity(
+	PetActivityID int NOT NULL IDENTITY,
 	PetID int NOT NULL,
+	ActivityType varchar(50) NOT NULL,
 	StartTime date NOT NULL,
 	EndTime date NOT NULL,
 	FoodConsumed float NOT NULL,
 	CONSTRAINT PK_PetActivity PRIMARY KEY (PetID)
-);
+)
 GO
 
 --Populate Users Table
 INSERT INTO Users
 VALUES
-(1, 'JSmith01', 'ILoveCats123'),
-(2, 'JDoe01', 'ILoveDigs321');
+('JSmith01', 'ILoveCats123'),
+('JDoe01', 'ILoveDigs321')
 GO
 
 --Populate Pet Table
 INSERT INTO Pet
 VALUES
-(1, 1, 'Doggo'),
-(2, 1, 'Catto'),
-(3, 2, 'Mr.Meowers');
+(1, 1, 'Doggo', 75, 75),
+(2, 1, 'Catto', 50, 75),
+(3, 2, 'Mr.Meowers', 60, 70)
 GO
 
 --Populate Pet Activity Table
 INSERT INTO PetActivity
 VALUES
-(1, CAST('2022-03-18 06:34:25 AM') AS 'datetime',
-CAST('2022-03-18 10:34:09 AM') AS 'datetime', 72),
-(2, CAST('2022-03-18 10:34:09 AM') AS 'datetime',
-CAST('2022-03-18 10:34:09 AM') AS 'datetime', 55);
+(1, 'Food', GETDATE(), GETDATE(), 72),
+(2, 'Water', GETDATE(), GETDATE(), 55)
 GO
