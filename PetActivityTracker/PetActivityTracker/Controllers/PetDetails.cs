ed91684ab3c989be650e43f3fa865df8e44823c7
wtotokshau1 <54911613+wtotokshau1@users.noreply.github.com>
2022-03-18 08:38:39 -0600

    Added Port to .NET 6.0 Framework

---
+++ PetActivityTracker/PetActivityTracker/Controllers/PetDetails.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetActivityTrackerMVC.Controllers
+{
+    public class PetDetails : Controller
+    {
+        public string Index()
+        {
+
+            return "In Pet Details";
+        }
+    }
+}
