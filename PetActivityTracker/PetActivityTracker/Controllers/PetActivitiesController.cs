511e4e0253cd84a81d9291397793c7790db33627
wtotokshau1 <54911613+wtotokshau1@users.noreply.github.com>
2022-03-30 16:02:46 -0600

    Improve pet activities page

--- PetActivityTracker/PetActivityTracker/Controllers/PetActivitiesController.cs
+++ PetActivityTracker/PetActivityTracker/Controllers/PetActivitiesController.cs
@@ -22,9 +22,37 @@ namespace PetActivityTracker.Models
         // GET: PetActivities
         public async Task<IActionResult> Index(int? id)
         {
-            var activities = _context.PetActivity.Where(pa => pa.PetId == id);
-            TempData["Pet"] = await _context.Pet.FirstOrDefaultAsync(pet=>pet.PetId==id);
+            //  Set current time frame to this day on first load
+            if (TempData["CurrentDate"] == null)   TempData["CurrentDate"] = DateTime.UtcNow;
+
+            //  Set ViewBad.Pet for View to display more pet information
+            ViewBag.Pet = await _context.Pet.FirstOrDefaultAsync(pet => pet.PetId == id);
+
+            //  Ensures Tempdata is kept between requests
+            TempData.Keep();
+            DateTime currTimeFrame = (DateTime)TempData["CurrentDate"];
+
+            var activities = _context.PetActivity.Where(pa => pa.PetId == id && pa.StartTime.DayOfYear == currTimeFrame.DayOfYear);
+            
             return View(await activities.ToListAsync());
+        }
+
+        public async Task<IActionResult> IncrementDay(int? id)
+        {
+            DateTime currTimeFrame = (DateTime)TempData["CurrentDate"];
+            TempData["CurrentDate"] = currTimeFrame.AddDays(1);
+            TempData.Keep();
+
+            return RedirectToAction("Index", new { id = id });
+        }
+
+        public async Task<IActionResult> DecrementDay(int? id)
+        {
+            DateTime currTimeFrame = (DateTime)TempData["CurrentDate"];
+            TempData["CurrentDate"] = currTimeFrame.AddDays(-1);
+            TempData.Keep();
+
+            return RedirectToAction("Index", new { id = id });
         }
 
         // GET: PetActivities/Details/5
